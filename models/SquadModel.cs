3b8c58787a96e3cfe16f78b8851d2ad47a1d613f
Riley-Mac <rileymitch117@gmail.com>
2025-03-21 10:28:09 +1100

    Squad implementation first

---
+++ models/SquadModel.cs
@@ -0,0 +1,15 @@
+namespace SwimmingAppBackend.Models
+{
+    public class Squad
+    {
+        public int id { get; set; }
+
+        required public int coachId { get; set; }
+        required public CoachProfile coachProfile { get; set; }
+
+        required public string squadName { get; set; }
+
+        public ICollection<SwimmerProfile>? swimmers { get; set; }
+        public ICollection<Set>? sets { get; set; }
+    }
+}
