a55815650a20983a5950d358f6c580078ef88f52
Riley-Mac <rileymitch117@gmail.com>
2025-03-21 12:18:48 +1100

    Added FK for squad to club

--- models/SquadModel.cs
+++ models/SquadModel.cs
@@ -7,6 +7,9 @@ namespace SwimmingAppBackend.Models
         required public int coachId { get; set; }
         required public CoachProfile coachProfile { get; set; }
 
+        public int? clubId { get; set; }
+        public Club? club { get; set; }
+
         required public string squadName { get; set; }
 
         public ICollection<SwimmerProfile>? swimmers { get; set; }
