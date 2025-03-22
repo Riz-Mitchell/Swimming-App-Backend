50f0afceae86a7b3c07b47d213a658bf7cf7551d
Riz-Mitchell <rileymitch117@gmail.com>
2025-03-22 19:25:44 +1100

    Uncommented swim controllers and fixed naming of Swims to swims

--- controllers/SwimController.cs
+++ controllers/SwimController.cs
@@ -1,99 +1,99 @@
-// using Microsoft.AspNetCore.Mvc;
-// using Microsoft.EntityFrameworkCore;
-// using SwimmingAppBackend.Models;
-// using SwimmingAppBackend.Context;
-// using System.Threading.Tasks;
-// using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using SwimmingAppBackend.Models;
+using SwimmingAppBackend.Context;
+using System.Threading.Tasks;
+using System.Linq;
 
-// namespace SwimmingAppBackend.Controllers
-// {
-//     [Route("api/[controller]")]
-//     [ApiController]
-//     public class SwimController : ControllerBase
-//     {
-//         private readonly SwimmingAppDBContext _context;
+namespace SwimmingAppBackend.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SwimController : ControllerBase
+    {
+        private readonly SwimmingAppDBContext _context;
 
-//         public SwimController(SwimmingAppDBContext context) => _context = context;
+        public SwimController(SwimmingAppDBContext context) => _context = context;
 
-//         // GET: api/swims
-//         [HttpGet]
-//         public async Task<ActionResult<IEnumerable<Swim>>> GetSwims()
-//             => await _context.Swims.ToListAsync();
+        // GET: api/swims
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Swim>>> GetSwims()
+            => await _context.swims.ToListAsync();
 
-//         // GET: api/swim/{id}
-//         [HttpGet("{id}")]
-//         public async Task<ActionResult<Swim>> GetSwim(int id)
-//         {
-//             var swim = await _context.Swims.FindAsync(id);
+        // GET: api/swim/{id}
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Swim>> GetSwim(int id)
+        {
+            var swim = await _context.swims.FindAsync(id);
 
-//             if (swim == null)
-//             {
-//                 return NotFound();
-//             }
+            if (swim == null)
+            {
+                return NotFound();
+            }
 
-//             return swim;
-//         }
+            return swim;
+        }
 
-//         // POST: api/swim
-//         [HttpPost]
-//         public async Task<ActionResult<Swim>> CreateSwim(Swim swim)
-//         {
-//             // Validate the swim model before adding to database
-//             if (!ModelState.IsValid)
-//             {
-//                 return BadRequest(ModelState);
-//             }
+        // POST: api/swim
+        [HttpPost]
+        public async Task<ActionResult<Swim>> CreateSwim(Swim swim)
+        {
+            // Validate the swim model before adding to database
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
 
-//             _context.Swims.Add(swim);
-//             await _context.SaveChangesAsync();
+            _context.swims.Add(swim);
+            await _context.SaveChangesAsync();
 
-//             return CreatedAtAction("GetSwim", new { id = swim.Id }, swim);
-//         }
+            return CreatedAtAction("GetSwim", new { id = swim.id }, swim);
+        }
 
-//         // PUT: api/swim/{id}
-//         [HttpPut("{id}")]
-//         public async Task<IActionResult> UpdateSwim(int id, Swim swim)
-//         {
-//             if (id != swim.Id)
-//             {
-//                 return BadRequest();
-//             }
+        // PUT: api/swim/{id}
+        [HttpPut("{id}")]
+        public async Task<IActionResult> UpdateSwim(int id, Swim swim)
+        {
+            if (id != swim.id)
+            {
+                return BadRequest();
+            }
 
-//             _context.Entry(swim).State = EntityState.Modified;
+            _context.Entry(swim).State = EntityState.Modified;
 
-//             try
-//             {
-//                 await _context.SaveChangesAsync();
-//             }
-//             catch (DbUpdateConcurrencyException)
-//             {
-//                 if (!_context.Swims.Any(e => e.Id == id))
-//                 {
-//                     return NotFound();
-//                 }
-//                 else
-//                 {
-//                     throw;
-//                 }
-//             }
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!_context.swims.Any(e => e.id == id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
 
-//             return NoContent();
-//         }
+            return NoContent();
+        }
 
-//         // DELETE: api/swim/{id}
-//         [HttpDelete("{id}")]
-//         public async Task<IActionResult> DeleteSwim(int id)
-//         {
-//             var swim = await _context.Swims.FindAsync(id);
-//             if (swim == null)
-//             {
-//                 return NotFound();
-//             }
+        // DELETE: api/swim/{id}
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteSwim(int id)
+        {
+            var swim = await _context.swims.FindAsync(id);
+            if (swim == null)
+            {
+                return NotFound();
+            }
 
-//             _context.Swims.Remove(swim);
-//             await _context.SaveChangesAsync();
+            _context.swims.Remove(swim);
+            await _context.SaveChangesAsync();
 
-//             return NoContent();
-//         }
-//     }
-// }
+            return NoContent();
+        }
+    }
+}
