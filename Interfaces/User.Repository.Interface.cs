7c34e5c1c007a6644c82181ac24495ed41f769bc
Riley-Mac <rileymitch117@gmail.com>
2025-03-29 23:25:34 +1100

    Implemented user repository interface

---
+++ Interfaces/User.Repository.Interface.cs
@@ -0,0 +1,18 @@
+using SwimmingAppBackend.DataTransferObjects;
+using SwimmingAppBackend.Models;
+
+namespace SwimmingAppBackend.Interfaces
+{
+    public interface IUserRepository
+    {
+        Task<List<User>> GetAllAsync();
+
+        Task<User> GetByIdAsync(int id);
+
+        Task CreateAsync(User user);
+
+        Task<User> UpdateAsync(int id, UpdateUserDTO updateUserDTO);
+
+        Task DeleteAsync(User user);
+    }
+}
