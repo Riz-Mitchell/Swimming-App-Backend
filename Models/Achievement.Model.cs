7c3b4ce82146a34c084bc5f3cb30643c234ea8e1
Riley-Mac <rileymitch117@gmail.com>
2025-04-05 17:07:45 +1100

    Name change

---
+++ Models/Achievement.Model.cs
@@ -0,0 +1,34 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace SwimmingAppBackend.Models
+{
+    public class Achievement
+    {
+        public int Id { get; set; }
+
+        // Attrubutes :
+        // ------------------------------------------------
+
+        public string? MainStroke { get; set; }
+
+        public int? MainDistance { get; set; }
+
+        public string? GoalTime { get; set; }
+
+        // ------------------------------------------------
+
+        // Foreign Keys / Relations:
+        // ++++++++++++++++++++++++++++++++++++++++++++++++
+
+        public required int UserOwnerId { get; set; }
+
+        public required User UserOwner { get; set; }
+
+        public ICollection<Swim>? Swims { get; set; }
+
+        public ICollection<Achievement>? Achievements { get; set; }
+
+        // ++++++++++++++++++++++++++++++++++++++++++++++++
+    }
+}
