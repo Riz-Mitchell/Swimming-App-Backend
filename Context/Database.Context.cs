67710ea032c665f2a7d7f04a48eb40bc75d42614
Riley-Mac <rileymitch117@gmail.com>
2025-04-06 11:09:26 +1000

    Defined new relationships

--- Context/Database.Context.cs
+++ Context/Database.Context.cs
@@ -8,51 +8,108 @@ namespace SwimmingAppBackend.Context
     {
         public SwimmingAppDBContext(DbContextOptions<SwimmingAppDBContext> options) : base(options) { }
 
+        public DbSet<Achievement> Achievements { get; set; }
+
+        public DbSet<AthleteData> AthleteDatas { get; set; }
+
+        public DbSet<Award> Awards { get; set; }
+
         public DbSet<Club> Clubs { get; set; }
+
+        public DbSet<CoachData> CoachDatas { get; set; }
+
+        public DbSet<Session> Sessions { get; set; }
+
+        public DbSet<Set> Sets { get; set; }
+
+        public DbSet<SetItem> SetItems { get; set; }
 
         public DbSet<Squad> Squads { get; set; }
 
+        public DbSet<Swim> Swims { get; set; }
+
+        public DbSet<TimeTable> TimeTables { get; set; }
+
         public DbSet<User> Users { get; set; }
 
-        public DbSet<SwimmerMetaData> SwimmerMetaDatas { get; set; }
-
-        public DbSet<CoachMetaData> CoachMetaDatas { get; set; }
-
-        public DbSet<Swim> Swims { get; set; }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
-            // modelBuilder.Entity<Squad>()
-            //     .HasOne(squad => squad.club)
-            //     .WithMany(club => club.squads)
-            //     .HasForeignKey(squad => squad.clubId);
 
+            modelBuilder.Entity<AthleteData>()
+                .HasOne(ad => ad.UserOwner)
+                .WithOne(uo => uo.AthleteData)
+                .HasForeignKey<AthleteData>(ad => ad.UserOwnerId)
+                .OnDelete(DeleteBehavior.Cascade);      // Dependency
+
+            modelBuilder.Entity<Award>()
+                .HasOne(a => a.CoachDataOwner)
+                .WithMany(cdo => cdo.Awards)
+                .HasForeignKey(a => a.CoachDataOwnerId)
+                .OnDelete(DeleteBehavior.Cascade);
+
+            modelBuilder.Entity<CoachData>()
+                .HasOne(cd => cd.UserOwner)
+                .WithOne(uo => uo.CoachData)
+                .HasForeignKey<CoachData>(cd => cd.UserOwnerId)
+                .OnDelete(DeleteBehavior.Cascade);
+
+            modelBuilder.Entity<Session>()
+                .HasOne(s => s.TimeTable)
+                .WithMany(tt => tt.Sessions)
+                .HasForeignKey(s => s.TimeTableId);
+
+            modelBuilder.Entity<Session>()
+                .HasOne(s => s.CoachDataOwner)
+                .WithMany(cdo => cdo.Sessions)
+                .HasForeignKey(s => s.CoachDataOwnerId)
+                .OnDelete(DeleteBehavior.Cascade);
+
+            modelBuilder.Entity<Set>()
+                .HasOne(s => s.Session)
+                .WithMany(sesh => sesh.Sets)
+                .HasForeignKey(s => s.SessionId)
+                .OnDelete(DeleteBehavior.Cascade);
+
+            modelBuilder.Entity<SetItem>()
+                .HasOne(si => si.Set)
+                .WithMany(s => s.SetItems)
+                .HasForeignKey(si => si.SetId)
+                .OnDelete(DeleteBehavior.Cascade);
+
+
+            // Squad can exist without a club
             modelBuilder.Entity<Squad>()
-                .HasOne(Squad => Squad.Club)
-                .WithMany(Club => Club.Squads)
-                .HasForeignKey(Squad => Squad.ClubId);
-
-            modelBuilder.Entity<User>()
-                .HasOne(User => User.Squad)
-                .WithMany(Squad => Squad.Members)
-                .HasForeignKey(User => User.SquadId);
-
-            modelBuilder.Entity<SwimmerMetaData>()
-                .HasOne(SwimmerMetaData => SwimmerMetaData.UserOwner)
-                .WithOne(User => User.SwimmerMetaData)
-                .HasForeignKey<SwimmerMetaData>(SwimmerMetaData => SwimmerMetaData.UserOwnerId);
-
-
-            modelBuilder.Entity<CoachMetaData>()
-                .HasOne(CoachMetaData => CoachMetaData.UserOwner)
-                .WithOne(User => User.CoachMetaData)
-                .HasForeignKey<CoachMetaData>(CoachMetaData => CoachMetaData.UserOwnerId);
+                .HasOne(s => s.Club)
+                .WithMany(c => c.Squads)
+                .HasForeignKey(s => s.ClubId);
 
             modelBuilder.Entity<Swim>()
-                .HasOne(Swim => Swim.SwimmerMetaData)
-                .WithMany(SwimmerMetaData => SwimmerMetaData.Swims)
-                .HasForeignKey(Swim => Swim.SwimmerMetaDataId);
+                .HasOne(s => s.AthleteDataOwner)
+                .WithMany(ado => ado.Swims)
+                .HasForeignKey(s => s.AthleteDataOwnerId)
+                .OnDelete(DeleteBehavior.Cascade);
 
+            modelBuilder.Entity<TimeTable>()
+                .HasOne(tt => tt.Squad)
+                .WithMany(s => s.TimeTables)
+                .HasForeignKey(tt => tt.SquadId)
+                .OnDelete(DeleteBehavior.Cascade);
+
+            modelBuilder.Entity<User>()
+                .HasOne(u => u.Squad)
+                .WithMany(s => s.Members)
+                .HasForeignKey(u => u.SquadId);
+
+            modelBuilder.Entity<User>()
+                .HasOne(u => u.AthleteData)
+                .WithOne(ad => ad.UserOwner)
+                .HasForeignKey<User>(u => u.AthleteDataId);
+
+            modelBuilder.Entity<User>()
+                .HasOne(u => u.CoachData)
+                .WithOne(ad => ad.UserOwner)
+                .HasForeignKey<User>(u => u.AthleteDataId);
         }
     }
 }
