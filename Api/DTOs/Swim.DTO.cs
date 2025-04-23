c4991e335f178d217b64962d5862d9b39a207a0e
Riz-Mitchell <rileymitch117@gmail.com>
2025-04-23 20:08:28 +1000

    Added Update Swim Req DTO and Get Swims Query

--- Api/DTOs/Swim.DTO.cs
+++ Api/DTOs/Swim.DTO.cs
@@ -1,38 +1,55 @@
 
 using SwimmingAppBackend.Enum;
-
-public class GetSwimResDTO
+namespace SwimmingAppBackend.Api.DTOs
 {
-    public required Guid Id { get; set; }
-    public required double Time { get; set; }
-    public required Stroke Stroke { get; set; } // Stroke type
-    public required int Distance { get; set; }
-    public double? PercentageOffPB { get; set; }
-    public double? PercentageOffGoalTime { get; set; }
-    public double? PercentageOffGoalStrokeRate { get; set; }
-    public double? PercentageOffGoalStrokeCount { get; set; }
-    public int? StrokeRate { get; set; }
-    public int? StrokeCount { get; set; }
-    public int? Pace { get; set; }
-    public int? PerceivedExertion { get; set; }
-    public required bool Dive { get; set; }
-    public DateTime RecordedAt { get; set; }
-}
+    public class GetSwimsQuery
+    {
+        public int PageNumber { get; set; } = 1;
+        public int? Distance { get; set; }
+        public Stroke? Stroke { get; set; }
+    }
 
-public class CreateSwimReqDTO
-{
-    public required double Time { get; set; }
-    public required Stroke Stroke { get; set; } // Stroke type
-    public required int Distance { get; set; }
-    public int? StrokeRate { get; set; }
-    public int? StrokeCount { get; set; }
-    public int? Pace { get; set; }
-    public int? PerceivedExertion { get; set; }
-    public required bool Dive { get; set; }
-    public DateTime RecordedAt { get; set; }
-}
+    public class GetSwimResDTO
+    {
+        public required Guid Id { get; set; }
+        public required double Time { get; set; }
+        public required Stroke Stroke { get; set; } // Stroke type
+        public required int Distance { get; set; }
+        public double? PercentageOffPB { get; set; }
+        public double? PercentageOffGoalTime { get; set; }
+        public double? PercentageOffGoalStrokeRate { get; set; }
+        public double? PercentageOffGoalStrokeCount { get; set; }
+        public int? StrokeRate { get; set; }
+        public int? StrokeCount { get; set; }
+        public int? Pace { get; set; }
+        public int? PerceivedExertion { get; set; }
+        public required bool Dive { get; set; }
+        public DateTime RecordedAt { get; set; }
+    }
+
+    public class CreateSwimReqDTO
+    {
+        public required double Time { get; set; }
+        public required Stroke Stroke { get; set; } // Stroke type
+        public required int Distance { get; set; }
+        public int? StrokeRate { get; set; }
+        public int? StrokeCount { get; set; }
+        public int? Pace { get; set; }
+        public int? PerceivedExertion { get; set; }
+        public required bool Dive { get; set; }
+        public DateTime RecordedAt { get; set; }
+    }
 
 
-public class UpdateSwimReqDTO
-{
+    public class UpdateSwimReqDTO
+    {
+        public double? Time { get; set; }
+        public Stroke? Stroke { get; set; } // Stroke type
+        public int? Distance { get; set; }
+        public int? StrokeRate { get; set; }
+        public int? StrokeCount { get; set; }
+        public int? Pace { get; set; }
+        public int? PerceivedExertion { get; set; }
+        public bool? Dive { get; set; }
+    }
 }
