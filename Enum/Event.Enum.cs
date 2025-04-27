941b071db5b1919889f1addb03c93a24856ae942
Riley-Mac <rileymitch117@gmail.com>
2025-04-27 17:00:48 +1000

    Added enum for all events

---
+++ Enum/Event.Enum.cs
@@ -0,0 +1,23 @@
+namespace SwimmingAppBackend.Enum
+{
+    public enum EventEnum
+    {
+        Freestyle50,
+        Freestyle100,
+        Freestyle200,
+        Freestyle400,
+        Freestyle800,
+        Freestyle1500,
+        Backstroke50,
+        Backstroke100,
+        Backstroke200,
+        Butterfly50,
+        Butterfly100,
+        Butterfly200,
+        Breaststroke50,
+        Breaststroke100,
+        Breaststroke200,
+        IndividualMedley200,
+        IndividualMedley400,
+    }
+}
