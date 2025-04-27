e0f27149635bf918bc603f2e03a4577945aa968f
Riley-Mac <rileymitch117@gmail.com>
2025-04-27 17:00:40 +1000

    Made changes to the swim DTOs

--- Api/DTOs/Swim.DTO.cs
+++ Api/DTOs/Swim.DTO.cs
@@ -11,25 +11,27 @@ namespace SwimmingAppBackend.Api.DTOs
 
     public class GetSwimResDTO
     {
-        public required Guid Id { get; set; }
+        public required Guid Id { get; set; }  // Primary Key
+        // public required EventEnum Event { get; set; } // Event type
         public required double Time { get; set; }
         public required Stroke Stroke { get; set; } // Stroke type
         public required int Distance { get; set; }
-        public double? PercentageOffPB { get; set; }
+        public double? PercentageOffPBTime { get; set; }
+        public double? PercentageOffPBStrokeRate { get; set; }
         public double? PercentageOffGoalTime { get; set; }
         public double? PercentageOffGoalStrokeRate { get; set; }
-        public double? PercentageOffGoalStrokeCount { get; set; }
         public int? StrokeRate { get; set; }
         public int? StrokeCount { get; set; }
         public int? Pace { get; set; }
         public int? PerceivedExertion { get; set; }
         public required bool Dive { get; set; }
-        public DateTime RecordedAt { get; set; }
-        public required Guid EventId { get; set; }
+        public required bool GoalSwim { get; set; }
+        public required DateTime RecordedAt { get; set; }
     }
 
     public class CreateSwimReqDTO
     {
+        public required EventEnum Event { get; set; } // Event type
         public required double Time { get; set; }
         public required Stroke Stroke { get; set; } // Stroke type
         public required int Distance { get; set; }
@@ -37,15 +39,14 @@ namespace SwimmingAppBackend.Api.DTOs
         public int? StrokeCount { get; set; }
         public int? Pace { get; set; }
         public int? PerceivedExertion { get; set; }
-        public required bool Dive { get; set; }
-        public DateTime RecordedAt { get; set; }
-        public required Guid AthleteDataOwnerId { get; set; }
-        public required Guid EventId { get; set; }
+        public bool? Dive { get; set; }
+        public bool? GoalSwim { get; set; }
     }
 
 
     public class UpdateSwimReqDTO
     {
+        public EventEnum? Event { get; set; } // Event type
         public double? Time { get; set; }
         public Stroke? Stroke { get; set; } // Stroke type
         public int? Distance { get; set; }
@@ -54,5 +55,6 @@ namespace SwimmingAppBackend.Api.DTOs
         public int? Pace { get; set; }
         public int? PerceivedExertion { get; set; }
         public bool? Dive { get; set; }
+        public bool? GoalSwim { get; set; }
     }
 }
