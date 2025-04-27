c426e47c4b69fbe9e4944086e0a74df1c49d4442
Riley-Mac <rileymitch117@gmail.com>
2025-04-27 17:01:21 +1000

    Moved percentage calculation logic

--- Infrastructure/Models/Swim.Model.cs
+++ Infrastructure/Models/Swim.Model.cs
@@ -13,50 +13,17 @@ namespace SwimmingAppBackend.Infrastructure.Models
         public required double Time { get; set; }
         public required Stroke Stroke { get; set; } // Stroke type
         public required int Distance { get; set; }
-        public double? PercentageOffPB
-        {
-            get
-            {
-                if (AthleteDataOwner?.Swims == null || AthleteDataOwner.Swims.Count == 0)
-                {
-                    return null;
-                }
-
-                var swimsInSameEvent = AthleteDataOwner.Swims
-                    .Where(s => s.EventId == this.EventId)
-                    .ToList();
-
-                var pbTime = swimsInSameEvent.Min(s => s.Time);
-
-                return ((this.Time - pbTime) / pbTime) * 100;
-            }
-        }
-        public double? PercentageOffGoalTime
-        {
-            get
-            {
-                if (AthleteDataOwner?.GoalSwims == null || AthleteDataOwner.GoalSwims.Count == 0)
-                {
-                    return null;
-                }
-
-                var goalTimes = AthleteDataOwner.GoalSwims
-                    .Where(s =>
-                        s.EventId == this.EventId
-                    )
-                    .ToList();
-
-                var goalTime = goalTimes.Min(s => s.Time);
-
-                return ((this.Time - goalTime) / goalTime) * 100;
-            }
-        }
+        public double? PercentageOffPBTime { get; set; }
+        public double? PercentageOffPBStrokeRate { get; set; }
+        public double? PercentageOffGoalTime { get; set; }
+        public double? PercentageOffGoalStrokeRate { get; set; }
         public int? StrokeRate { get; set; }
         public int? StrokeCount { get; set; }
         public int? Pace { get; set; }
         public int? PerceivedExertion { get; set; }
         public required bool Dive { get; set; }
         public DateTime RecordedAt { get; set; } = DateTime.UtcNow;
+        public bool GoalSwim { get; set; } = false;
 
         // ------------------------------------------------
 
