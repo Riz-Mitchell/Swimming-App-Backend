e95a705beab3adbb0863034e1e9156f1884854af
Riz-Mitchell <rileymitch117@gmail.com>
2025-04-29 21:41:46 +1000

    Function to help calculate split times

---
+++ Domain/Helpers/Interpolation.Helper.cs
@@ -0,0 +1,45 @@
+namespace SwimmingAppBackend.Domain.Helpers
+{
+    public static class InterpolationHelper
+    {
+        public static double PredictTotalTime(List<SplitData> data, double splitTime, int distance)
+        {
+            var ordered = data
+                .Where(d => d.SplitsByDistance.ContainsKey(distance))
+                .OrderBy(d => d.SplitsByDistance[distance])
+                .ToList();
+
+            for (int i = 0; i < ordered.Count - 1; i++)
+            {
+                double t1 = ordered[i].SplitsByDistance[distance];
+                double t2 = ordered[i + 1].SplitsByDistance[distance];
+
+                if (splitTime >= t1 && splitTime <= t2)
+                {
+                    double y1 = ordered[i].TotalTime;
+                    double y2 = ordered[i + 1].TotalTime;
+
+                    // Linear interpolation
+                    return y1 + (splitTime - t1) * (y2 - y1) / (t2 - t1);
+                }
+            }
+
+            // Outside known range – scaling from nearest point
+            var nearest = ordered
+                .OrderBy(d => Math.Abs(d.SplitsByDistance[distance] - splitTime))
+                .First();
+
+            double nearestSplit = nearest.SplitsByDistance[distance];
+            double nearestTotal = nearest.TotalTime;
+
+            // Scale total time proportionally
+            return nearestTotal * (splitTime / nearestSplit);
+        }
+    }
+
+    public class SplitData
+    {
+        public double TotalTime { get; set; }
+        public Dictionary<int, double> SplitsByDistance { get; set; } = new();
+    }
+}
