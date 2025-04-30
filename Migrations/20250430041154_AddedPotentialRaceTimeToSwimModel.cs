b884fccfba89b3927d4d0eb9ad960694e704d9c3
Riz-Mitchell <rileymitch117@gmail.com>
2025-04-30 14:40:44 +1000

    New migrations

---
+++ Migrations/20250430041154_AddedPotentialRaceTimeToSwimModel.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Swimming_App_Backend.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddedPotentialRaceTimeToSwimModel : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<double>(
+                name: "PotentialRaceTime",
+                table: "Swims",
+                type: "double precision",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "PotentialRaceTime",
+                table: "Swims");
+        }
+    }
+}
