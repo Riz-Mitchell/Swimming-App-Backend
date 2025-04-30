8615c5c39ddb53173d8078529090e159fc21b0f9
Riz-Mitchell <rileymitch117@gmail.com>
2025-04-30 14:39:27 +1000

    Removed Pace property and ensured PotentialRaceTime is included in Swim model

--- Infrastructure/Models/Swim.Model.cs
+++ Infrastructure/Models/Swim.Model.cs
@@ -18,9 +18,9 @@ namespace SwimmingAppBackend.Infrastructure.Models
         public double? PercentageOffPBStrokeRate { get; set; }
         public double? PercentageOffGoalTime { get; set; }
         public double? PercentageOffGoalStrokeRate { get; set; }
+        public double? PotentialRaceTime { get; set; }
         public int? StrokeRate { get; set; }
         public int? StrokeCount { get; set; }
-        public int? Pace { get; set; }
         public int? PerceivedExertion { get; set; }
         public required bool Dive { get; set; }
         public DateTime RecordedAt { get; set; } = DateTime.UtcNow;
