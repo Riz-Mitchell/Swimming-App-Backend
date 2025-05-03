93a54307bb4408047129ced68afccdddff3e7e51
Riz-Mitchell <rileymitch117@gmail.com>
2025-05-03 11:28:13 +1000

    Add UserAchievement model with properties for tracking user achievements

---
+++ Infrastructure/Models/UserAchievement.Model.cs
@@ -0,0 +1,29 @@
+namespace SwimmingAppBackend.Infrastructure.Models
+{
+    public class UserAchievement
+    {
+        public Guid Id { get; set; }
+
+        // Attributes :
+        // ------------------------------------------------
+
+        public int Pgrogress { get; set; } = 0;
+
+        public bool IsCompleted { get; set; } = false;
+
+        public DateTime? EarnedAt { get; set; }
+
+        // ------------------------------------------------
+
+        // Foreign Keys / Relations:
+        // ++++++++++++++++++++++++++++++++++++++++++++++++
+
+        public required Guid UserId { get; set; }
+        public User User { get; set; } = null!;
+
+        public required Guid AchievementId { get; set; }
+        public Achievement Achievement { get; set; } = null!;
+
+        // ++++++++++++++++++++++++++++++++++++++++++++++++
+    }
+}
