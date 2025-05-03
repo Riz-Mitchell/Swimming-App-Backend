bc8d2f6c11283d39a235a25f0c2bdb1babf718c1
Riz-Mitchell <rileymitch117@gmail.com>
2025-05-03 11:27:53 +1000

    Add properties to Achievement model for description, target value, difficulty, and user type

--- Infrastructure/Models/Achievement.Model.cs
+++ Infrastructure/Models/Achievement.Model.cs
@@ -1,5 +1,6 @@
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
+using SwimmingAppBackend.Enum;
 
 namespace SwimmingAppBackend.Infrastructure.Models
 {
@@ -12,12 +13,21 @@ namespace SwimmingAppBackend.Infrastructure.Models
 
         public required string Name { get; set; }
 
+        public required string Description { get; set; }
+
+        public required int TargetValue { get; set; }
+
+        [Range(1, 5)]
+        public required int Difficulty { get; set; }
+
+        public required UserType UserType { get; set; }
+
         // ------------------------------------------------
 
         // Foreign Keys / Relations:
         // ++++++++++++++++++++++++++++++++++++++++++++++++
 
-
+        public ICollection<UserAchievement> UserAchievements { get; set; } = [];
 
         // ++++++++++++++++++++++++++++++++++++++++++++++++
     }
