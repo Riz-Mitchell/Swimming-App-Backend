7be14792579af942f15d0c941ac83d342379d80d
Riz-Mitchell <rileymitch117@gmail.com>
2025-05-10 23:03:31 +1000

    Refactor UserAchievementOrderBy enum and update default query parameters

--- Api/DTOs/UserAchievements.DTO.cs
+++ Api/DTOs/UserAchievements.DTO.cs
@@ -1,18 +1,31 @@
+using SwimmingAppBackend.Enum;
+
 namespace SwimmingAppBackend.Api.DTOs
 {
     public enum UserAchievementOrderBy
     {
-        ProgressAsc,
-        ProgressDesc,
-        DifficultyAsc,
-        DifficultyDesc,
+        Progress,
+        Difficulty,
     }
 
     public class UserAchievementsQuery
     {
         public Guid? UserId { get; set; }
-        public UserAchievementOrderBy OrderBy { get; set; } = UserAchievementOrderBy.ProgressAsc;
+        public UserAchievementOrderBy OrderBy { get; set; } = UserAchievementOrderBy.Progress;
+        public bool IsDescending { get; set; } = false;
         public int Page { get; set; } = 1;
         public int PageSize { get; set; } = 20;
+    }
+
+    public class GetUserAchievementResDTO
+    {
+        public required Guid Id { get; set; }
+        public required string Name { get; set; }
+        public required string Description { get; set; }
+        public required int TargetValue { get; set; }
+        public required int Progress { get; set; }
+        public required bool IsCompleted { get; set; }
+        public required DateTime? EarnedAt { get; set; }
+        public required int Difficulty { get; set; }
     }
 }
