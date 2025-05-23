0c5d71f69a3c920abcfc8c25d79eeee7a94999aa
Riley-Mac <rileymitch117@gmail.com>
2025-05-23 17:19:28 +1000

    Add Friendship model to represent user connections

---
+++ Infrastructure/Models/Friendship.Model.cs
@@ -0,0 +1,17 @@
+namespace SwimmingAppBackend.Infrastructure.Models
+{
+
+    public class Friendship
+    {
+        public Guid Id { get; set; }
+
+        public required Guid RequesterId { get; set; }
+        public required User Requester { get; set; }
+
+        public required Guid AddresseeId { get; set; }
+        public required User Addressee { get; set; }
+
+        public DateTime RequestedAt { get; set; } = DateTime.UtcNow;
+        public bool IsConfirmed { get; set; } = false;
+    }
+}
