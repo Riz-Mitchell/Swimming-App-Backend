8336ca5d2a22090c9cb919613c2704f7fab4e76e
Riz-Mitchell <rileymitch117@gmail.com>
2025-06-26 16:42:14 +1000

    Refactor Swim and Split models: Remove unnecessary properties and add Split model for detailed swim splits

---
+++ Infrastructure/Models/Split.Model.cs
@@ -0,0 +1,33 @@
+using SwimmingAppBackend.Enum;
+
+namespace SwimmingAppBackend.Infrastructure.Models
+{
+    public class Split
+    {
+        public Guid Id { get; set; }  // Primary Key
+
+        // Attributes:
+        // ------------------------------------------------
+        public Stroke Stroke { get; set; } // Stroke type for this split
+        public required double IntervalTime { get; set; } // Time for this split in seconds
+        public required int IntervalDistance { get; set; } // Distance for this split in meters
+        public int? IntervalStrokeRate { get; set; } // Stroke rate for this split
+        public int? IntervalStrokeCount { get; set; } // Stroke count for this split
+        public double? PerOffPBIntervalTime { get; set; } // Percentage off personal best time for this split
+        public double? PerOffPBStrokeRate { get; set; }
+        public double? PerOffGoalTime { get; set; } // Percentage off goal time for this split
+        public double? PerOffGoalStrokeRate { get; set; } // Percentage off goal stroke rate for this split
+        public required bool Dive { get; set; } // Indicates if this split was from a dive start
+        public DateTime RecordedAt { get; set; } = DateTime.UtcNow;
+
+        // ------------------------------------------------
+
+        // Foreign Keys / Relations:
+        // ++++++++++++++++++++++++++++++++++++++++++++++++
+
+        public required Guid SwimId { get; set; }
+        public Swim Swim { get; set; }
+
+        // ++++++++++++++++++++++++++++++++++++++++++++++++
+    }
+}
--- Infrastructure/Models/Swim.Model.cs
+++ Infrastructure/Models/Swim.Model.cs
@@ -9,20 +9,8 @@ namespace SwimmingAppBackend.Infrastructure.Models
 
         // Attrubutes :
         // ------------------------------------------------
-
-        public required double Time { get; set; }
-        public required Stroke Stroke { get; set; } // Stroke type
-        public required int Distance { get; set; }
         public required EventEnum Event { get; set; } // E.g: 50m Freestyle
-        public double? PercentageOffPBTime { get; set; }
-        public double? PercentageOffPBStrokeRate { get; set; }
-        public double? PercentageOffGoalTime { get; set; }
-        public double? PercentageOffGoalStrokeRate { get; set; }
-        public double? PotentialRaceTime { get; set; }
-        public int? StrokeRate { get; set; }
-        public int? StrokeCount { get; set; }
         public int? PerceivedExertion { get; set; }
-        public required bool Dive { get; set; }
         public DateTime RecordedAt { get; set; } = DateTime.UtcNow;
         public bool GoalSwim { get; set; } = false;
 
@@ -30,6 +18,8 @@ namespace SwimmingAppBackend.Infrastructure.Models
 
         // Foreign Keys / Relations:
         // ++++++++++++++++++++++++++++++++++++++++++++++++
+
+        public required ICollection<Split> Splits { get; set; } = [];
 
         public required Guid AthleteDataOwnerId { get; set; }
 
