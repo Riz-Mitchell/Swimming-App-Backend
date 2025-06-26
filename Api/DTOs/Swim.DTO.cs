6643f6c50d644e70e90e3e3814ee49bd81d5c0ef
Riz-Mitchell <rileymitch117@gmail.com>
2025-06-26 16:41:28 +1000

    Refactor Swim DTOs: Remove unnecessary properties and integrate splits for swim details

--- Api/DTOs/Swim.DTO.cs
+++ Api/DTOs/Swim.DTO.cs
@@ -21,18 +21,9 @@ namespace SwimmingAppBackend.Api.DTOs
     {
         public required Guid Id { get; set; }  // Primary Key
         public required EventEnum Event { get; set; } // Event type
-        public required double Time { get; set; }
-        public required Stroke Stroke { get; set; } // Stroke type
-        public required int Distance { get; set; }
-        public double? PercentageOffPBTime { get; set; }
-        public double? PercentageOffPBStrokeRate { get; set; }
-        public double? PercentageOffGoalTime { get; set; }
-        public double? PercentageOffGoalStrokeRate { get; set; }
-        public double? PotentialRaceTime { get; set; }
-        public int? StrokeRate { get; set; }
-        public int? StrokeCount { get; set; }
+        public required List<GetSplitResDTO> Splits { get; set; }
+
         public int? PerceivedExertion { get; set; }
-        public required bool Dive { get; set; }
         public required bool GoalSwim { get; set; }
         public required DateTime RecordedAt { get; set; }
     }
@@ -40,27 +31,16 @@ namespace SwimmingAppBackend.Api.DTOs
     public class CreateSwimReqDTO
     {
         public required EventEnum Event { get; set; } // Event type
-        public required double Time { get; set; }
-        public required Stroke Stroke { get; set; } // Stroke type
-        public required int Distance { get; set; }
-        public int? StrokeRate { get; set; }
-        public int? StrokeCount { get; set; }
         public int? PerceivedExertion { get; set; }
-        public bool? Dive { get; set; }
-        public bool? GoalSwim { get; set; }
+        public required List<CreateSplitReqDTO> Splits { get; set; } = [];
+        public bool GoalSwim { get; set; } = false;
     }
 
 
     public class UpdateSwimReqDTO
     {
         public EventEnum? Event { get; set; } // Event type
-        public double? Time { get; set; }
-        public Stroke? Stroke { get; set; } // Stroke type
-        public int? Distance { get; set; }
-        public int? StrokeRate { get; set; }
-        public int? StrokeCount { get; set; }
         public int? PerceivedExertion { get; set; }
-        public bool? Dive { get; set; }
         public bool? GoalSwim { get; set; }
     }
 }
