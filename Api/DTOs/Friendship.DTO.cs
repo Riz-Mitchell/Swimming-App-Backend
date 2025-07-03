cd1acc6f6a6ffabb577015ca0abe48abdd01f982
Riz-Mitchell <rileymitch117@gmail.com>
2025-07-03 15:26:56 +1000

    Add GetFriendshipQuery DTO and FriendshipMapper for mapping Friendship entities

--- Api/DTOs/Friendship.DTO.cs
+++ Api/DTOs/Friendship.DTO.cs
@@ -1,5 +1,14 @@
+using SwimmingAppBackend.Infrastructure.Models;
+
 namespace SwimmingAppBackend.Api.DTOs
 {
+    public class GetFriendshipQuery
+    {
+        public Guid UserId { get; set; }
+        public int Page { get; set; } = 1;
+        public int PageSize { get; set; } = 20;
+    }
+
     public class GetFriendshipResDTO
     {
         public Guid Id { get; set; }
@@ -18,5 +27,31 @@ namespace SwimmingAppBackend.Api.DTOs
     {
         public required Guid FriendshipId { get; set; }
         public required bool IsConfirmed { get; set; }
+    }
+
+    public static class FriendshipMapper
+    {
+        public static GetFriendshipResDTO ModelToRes(Friendship modelObj)
+        {
+            return new GetFriendshipResDTO
+            {
+                Id = modelObj.Id,
+                RequesterId = modelObj.RequesterId,
+                AddresseeId = modelObj.AddresseeId,
+                IsConfirmed = modelObj.IsConfirmed
+            };
+        }
+
+        public static List<GetFriendshipResDTO> ListModelToListRes(List<Friendship> modelList)
+        {
+            List<GetFriendshipResDTO> returnList = [];
+
+            foreach (Friendship modelObj in modelList)
+            {
+                returnList.Add(ModelToRes(modelObj));
+            }
+
+            return returnList;
+        }
     }
 }
