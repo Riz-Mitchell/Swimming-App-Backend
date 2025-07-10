935200d6b1e19c68d2188cd3aefe16552bc1d07f
Riz-Mitchell <rileymitch117@gmail.com>
2025-07-10 11:51:54 +1000

    Further validation for events and strokes

--- Api/Validators/Swim.Validator.cs
+++ Api/Validators/Swim.Validator.cs
@@ -1,5 +1,6 @@
 using FluentValidation;
 using SwimmingAppBackend.Api.DTOs;
+using SwimmingAppBackend.Domain.Helpers;
 
 namespace SwimmingAppBackend.Api.Validators
 {
@@ -28,6 +29,44 @@ namespace SwimmingAppBackend.Api.Validators
 
             RuleForEach(s => s.Splits)
                 .SetValidator(new CreateSplitReqValidator());
+
+            RuleFor(s => s)
+                .Custom((swim, context) =>
+                {
+                    int eventDistance = EventHelper.GetDistance(swim.Event);
+
+                    foreach (var (split, index) in swim.Splits.Select((s, i) => (s, i)))
+                    {
+                        int splitDistance = split.IntervalDistance;
+
+                        if (!IsValidSplitDistance(eventDistance, splitDistance))
+                        {
+                            context.AddFailure($"Splits[{index}].IntervalDistance",
+                                $"Invalid split distance {splitDistance}m for {eventDistance}m event.");
+                        }
+
+                        if (!EventHelper.IsStrokeValid(split.Stroke, swim.Event))
+                        {
+                            context.AddFailure($"Splits[{index}].Stroke",
+                                $"Invalid split stroke {split.Stroke} for {swim.Event} event");
+                        }
+                    }
+                });
+        }
+
+        private bool IsValidSplitDistance(int eDist, int sDist)
+        {
+            if (sDist > eDist)
+            {
+                return false;
+            }
+
+            if (eDist <= 100)
+                return sDist % 5 == 0;
+            else if (eDist <= 400)
+                return sDist % 25 == 0;
+            else
+                return sDist % 50 == 0;
         }
     }
 
