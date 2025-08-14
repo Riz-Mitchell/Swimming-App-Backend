cf5fc2ed72a05d213136e2d11d2cd5b38dc9e444
Riley-Mac <rileymitch117@gmail.com>
2025-08-14 18:17:58 +1000

    Refactor SwimQuestionnaire DTO to use lists for Nerves, CatchFeel, HeadPosition, and Turn attributes

--- Api/DTOs/SwimQuestionnaire.DTO.cs
+++ Api/DTOs/SwimQuestionnaire.DTO.cs
@@ -9,15 +9,15 @@ namespace SwimmingAppBackend.Api.DTOs
         // Attributes:
         // ------------------------------------------------
         public required SelfTalkOptionsEnum SelfTalk { get; set; }
-        public required NervesOptionsEnum Nerves { get; set; }
+        public required List<NervesOptionsEnum> Nerves { get; set; }
         public required EnergyLevelOptionsEnum EnergyLevel { get; set; }
         public required BreathingOptionsEnum Breathing { get; set; }
-        public required CatchFeelOptionsEnum CatchFeel { get; set; }
+        public required List<CatchFeelOptionsEnum> CatchFeel { get; set; }
         public required StrokeLengthOptionsEnum StrokeLength { get; set; }
         public required KickTechniqueOptionsEnum KickTechnique { get; set; }
         public required KickThroughoutOptionsEnum KickThroughout { get; set; }
-        public required HeadPositionOptionsEnum HeadPosition { get; set; }
-        public required TurnOptionsEnum Turn { get; set; }
+        public required List<HeadPositionOptionsEnum> HeadPosition { get; set; }
+        public required List<TurnOptionsEnum> Turn { get; set; }
     }
 
     public class CreateSwimQuestionnaireReqDTO
@@ -25,29 +25,29 @@ namespace SwimmingAppBackend.Api.DTOs
         // Attributes:
         // ------------------------------------------------
         public required SelfTalkOptionsEnum SelfTalk { get; set; }
-        public required NervesOptionsEnum Nerves { get; set; }
+        public required List<NervesOptionsEnum> Nerves { get; set; }
         public required EnergyLevelOptionsEnum EnergyLevel { get; set; }
         public required BreathingOptionsEnum Breathing { get; set; }
-        public required CatchFeelOptionsEnum CatchFeel { get; set; }
+        public required List<CatchFeelOptionsEnum> CatchFeel { get; set; }
         public required StrokeLengthOptionsEnum StrokeLength { get; set; }
         public required KickTechniqueOptionsEnum KickTechnique { get; set; }
         public required KickThroughoutOptionsEnum KickThroughout { get; set; }
-        public required HeadPositionOptionsEnum HeadPosition { get; set; }
-        public required TurnOptionsEnum Turn { get; set; }
+        public required List<HeadPositionOptionsEnum> HeadPosition { get; set; }
+        public required List<TurnOptionsEnum> Turn { get; set; }
     }
 
     public class UpdateSwimQuestionnaireReqDTO
     {
         public SelfTalkOptionsEnum? SelfTalk { get; set; }
-        public NervesOptionsEnum? Nerves { get; set; }
+        public List<NervesOptionsEnum>? Nerves { get; set; }
         public EnergyLevelOptionsEnum? EnergyLevel { get; set; }
         public BreathingOptionsEnum? Breathing { get; set; }
-        public CatchFeelOptionsEnum? CatchFeel { get; set; }
+        public List<CatchFeelOptionsEnum>? CatchFeel { get; set; }
         public StrokeLengthOptionsEnum? StrokeLength { get; set; }
         public KickTechniqueOptionsEnum? KickTechnique { get; set; }
         public KickThroughoutOptionsEnum? KickThroughout { get; set; }
-        public HeadPositionOptionsEnum? HeadPosition { get; set; }
-        public TurnOptionsEnum? Turn { get; set; }
+        public List<HeadPositionOptionsEnum>? HeadPosition { get; set; }
+        public List<TurnOptionsEnum>? Turn { get; set; }
     }
 
     public static class SwimQuestionnaireMapper
